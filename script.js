4c42a3c20d109e53f7c8943833525e226e154967
David Estrada <davidandresestrada15@gmail.com>
2025-08-13 12:35:07 -0500

    Se termino la funcionalidad y se mejoro el diseño de la lista de usuarios

--- script.js
+++ script.js
@@ -6,56 +6,92 @@ const infoimage= document.getElementById("infoimage")
 const accountsbutton= document.getElementById("accounts")
 const buttonWindow= document.createElement("button")
 const buttonkill= document.createElement("button")
+buttonkill.classList.add("buttonkill")
+const closeloggin= document.createElement("button")
+closeloggin.innerText= "Cerrar sesion"
 const Agetext= document.getElementById("Agetext")
 const Nametext= document.getElementById("Nametext")
 const Emailtext= document.getElementById("Emailtext")
 buttonWindow.textContent= "Crear nuevo usuario"
 buttonkill.textContent= "X"
-accountsbutton.addEventListener("click", ()=>{
-    
-    const section = document.createElement("section")
+let loggin= JSON.parse(localStorage.getItem("logginstatus")) || false
+if (loggin  == false) {
+    main.style.display= "none"
+    closeloggin.style.display= "none"
+    buttonkill.style.display= "none"
+    createmodel()
+}
+
+closeloggin.addEventListener("click", ()=>{
+    loggin= false
+    JSON.stringify(localStorage.setItem("logginstatus", loggin))
+    location.reload()
+})
+
+actualizarusuario()
+
+function createmodel() {
+const section = document.createElement("section")
     section.style.animation= " aparecer 0.5s ease both";
     const accountscontainer = document.createElement("div")
     const buttonscontainer= document.createElement("div")
-    controlador.getAvailableUsers().forEach(Usuario => {
+    checkloggin().forEach(Usuario => {
         const account= document.createElement("article")
+        account.classList.add("account")
+        const infocontainer= document.createElement("div")
+        infocontainer.classList.add("infocontainer")
+        const infotextcontainer= document.createElement("div")
         const name= document.createElement("h2")
         const age= document.createElement("h3")
         const email= document.createElement("h3")
         const img= document.createElement("img")
         const eraseacount= document.createElement("button")
+        eraseacount.classList.add("eraseacount")
         name.textContent= Usuario.nombre
         age.textContent= Usuario.edad
         email.textContent= Usuario.correo
         img.src= Usuario.image
-
-        img.addEventListener("click", () =>{
+        eraseacount.innerText= "Borrar usuario"
+        infocontainer.addEventListener("click", () =>{
         controlador.changeUser(Usuario.id)
         actualizarusuario()
         section.style.animation= " desaparecer 0.5s ease both";
-        })
+        loggin = true
+        JSON.stringify(localStorage.setItem("logginstatus", loggin))
+        main.style.display= "flex"
+        closeloggin.style.display= "block"
+        buttonkill.style.display= "block"})
 
         eraseacount.addEventListener("click", ()=>{
             controlador.removeUsers(Usuario)
-        document.body.removeChild(section)     })
-        account.appendChild(eraseacount)
-        account.appendChild(name)
-        account.appendChild(age)
-        account.appendChild(email)
-        account.appendChild(img)
+        section.style.animation= " desaparecer 0.5s ease both";    })
+        infocontainer.appendChild(img)
+        infocontainer.appendChild(infotextcontainer)
+        infotextcontainer.appendChild(name)
+        infotextcontainer.appendChild(email)
+        
+        account.appendChild(infocontainer)
+        if (loggin == true) {
+          account.appendChild(eraseacount)  
+        }
         
         accountscontainer.appendChild(account)
     });
     accountscontainer.classList.add("accountscontainer")
     buttonscontainer.appendChild(buttonWindow)
     buttonscontainer.appendChild(buttonkill)
+    buttonscontainer.appendChild(closeloggin)
     buttonscontainer.classList.add("buttonscontainer")
     section.appendChild(buttonscontainer)
     section.appendChild(accountscontainer)
     document.body.appendChild(section)
     buttonkill.addEventListener("click", ()=>{
         section.style.animation= " desaparecer 0.5s ease both";
-})
+})    
+}
+
+accountsbutton.addEventListener("click", ()=>{
+createmodel()
 
 })
 
@@ -77,4 +113,6 @@ function actualizarusuario() {
     Nametext.innerText= controlador.actualUser.nombre
 }
 
-actualizarusuario()
+function checkloggin() {
+    return loggin == false ? controlador.getAllusers() : controlador.getAvailableUsers()
+}
